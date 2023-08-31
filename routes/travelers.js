4a902aedba2364ac7997cadf9229e8af6d9c6b80
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-31 00:24:42 -0400

    started routes for travelers

--- routes/travelers.js
+++ routes/travelers.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex")(require("../knexfile").development);
+
+router.use(express.json())
+
+router.get("/", async (_req, res) => {
+    try {
+        const travelers = await knex("users");
+        res.status(200).json(travelers);
+    } catch (error) {
+        console.error("Error retrieving travelers", error);
+        res.status(500).json({ error: "Internal server error" });
+
+    }
+})
 
 module.exports = router;
